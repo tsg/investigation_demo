ba8e31f15508b6d83ad11f826a28a2e75a440b63
Tudor Golubenco <tudor@elastic.co>
2019-05-09 11:42:53 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# ensure right directory and virtualenv
+base=$(dirname "$0")
+source $base/env/bin/activate
+cd $base
+
 # three times failures to get enough of them
-HOUR_ADJUST=-2 ./load.py bob_failures.json
-HOUR_ADJUST=-2 ./load.py bob_failures.json
-HOUR_ADJUST=-2 ./load.py bob_failures.json
+HOUR_ADJUST=-3 ./load.py bob_failures.json
+HOUR_ADJUST=-3 ./load.py bob_failures.json
+HOUR_ADJUST=-3 ./load.py bob_failures.json
 
 # one time success
 HOUR_ADJUST=-2 ./load.py bob_success.json
 
 # one time workflow, after everything else but still in the past
-HOUR_ADJUST=-1 ./load.py saved_*.json
+HOUR_ADJUST=-1 ./load.py saved_events_auditbeat.json saved_events_packetbeat.json
 
